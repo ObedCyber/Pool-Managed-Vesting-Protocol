29c450e1636b3a0247e20b688e7cf00261b2a605
burata <buratabariziga@gmail.com>
2025-12-14 21:22:44 +0100

    Updated VestingShares.sol

--- src/VestingShares.sol
+++ src/VestingShares.sol
@@ -1,9 +1,6 @@
 // SPDX-license-Identifier: MIT
 pragma solidity ^0.8.30;
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VestingShares is ERC20 {
