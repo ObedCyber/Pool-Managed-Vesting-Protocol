aa648b3a6c8723a9753db7448712b3ba48068d72
obedcyber <obedokoh622@gmail.com>
2025-12-18 15:20:11 +0100

    Added Liquidity Manager & Treasury

---
+++ src/interfaces/IUniswapV3Factory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+interface IUniswapV3Factory {
+    function getPool(
+        address token0,
+        address token1,
+        uint24 fee
+    ) external view returns (address);
+}
